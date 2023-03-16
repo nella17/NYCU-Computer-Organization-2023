a76451f0c5415d779f6fa1faa5929f0644942108
Allen Guan <me@nella17.tw>
2023-03-16 19:48:36 +0800

    [HW1] localparam instead define

--- HW1/alu.v
+++ HW1/alu.v
@@ -1,10 +1,3 @@
-`define OP_AND 4'b0000
-`define OP_OR  4'b0001
-`define OP_ADD 4'b0010
-`define OP_SUB 4'b0110
-`define OP_NOR 4'b1100
-`define OP_SLT 4'b0111
-
 module alu #(parameter DWIDTH = 32)
 (
     input [3 : 0] op,           // Operation to perform.
@@ -14,6 +7,13 @@ module alu #(parameter DWIDTH = 32)
     output zero,                // zero = 1 if rd is 0, 0 otherwise.
     output overflow             // overflow = 1 if overflow happens.
 );
+
+    localparam [3:0] OP_AND = 4'b0000,
+                     OP_OR  = 4'b0001,
+                     OP_ADD = 4'b0010,
+                     OP_SUB = 4'b0110,
+                     OP_NOR = 4'b1100,
+                     OP_SLT = 4'b0111;
 
     reg valid;
     always @(*) begin
