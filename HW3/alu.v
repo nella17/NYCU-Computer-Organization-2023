f9327613faf2c8f18ead2ef6d0fb90cd41b9aefc
Allen Guan <me@nella17.tw>
2023-04-20 16:08:59 +0800

    [HW3] copy file

--- HW3/alu.v
+++ HW3/alu.v
@@ -1,1 +1,58 @@
-../HW1/alu.v
+module alu #(parameter DWIDTH = 32)
+(
+    input [3 : 0] op,           // Operation to perform.
+    input signed [DWIDTH-1 : 0] rs1,   // Input data #1.
+    input signed [DWIDTH-1 : 0] rs2,   // Input data #2.
+    output reg signed [DWIDTH-1 : 0] rd,   // Result of computation.
+    output zero,                // zero = 1 if rd is 0, 0 otherwise.
+    output overflow             // overflow = 1 if overflow happens.
+);
+
+    localparam [3:0] OP_AND = 4'b0000,
+                     OP_OR  = 4'b0001,
+                     OP_ADD = 4'b0010,
+                     OP_SUB = 4'b0110,
+                     OP_NOR = 4'b1100,
+                     OP_SLT = 4'b0111;
+
+    reg valid;
+    always @(*) begin
+        casez (op)
+            OP_AND,
+            OP_OR ,
+            OP_ADD,
+            OP_SUB,
+            OP_NOR,
+            OP_SLT:
+                valid = 1;
+            default:
+                valid = 0;
+        endcase
+    end
+
+    assign zero = valid && ~overflow && rd == 0;
+
+    always @(*) begin
+        casez (op)
+            OP_AND: rd = rs1 & rs2;
+            OP_OR : rd = rs1 | rs2;
+            OP_ADD: rd = rs1 + rs2;
+            OP_SUB: rd = rs1 - rs2;
+            OP_NOR: rd = ~(rs1 | rs2);
+            OP_SLT: rd = { {(DWIDTH-1){ 1'h0 }}, rs1 < rs2 };
+            default: rd = 0;
+        endcase
+    end
+
+    always @(*) begin
+        casez (op)
+            OP_ADD:
+                overflow = (rs1 >= 0 && rs2 >= 0 && rd < 0) || (rs1 < 0 && rs2 < 0 && rd >= 0);
+            OP_SUB:
+                overflow = (rs1 >= 0 && rs2 < 0 && rd < 0) || (rs1 < 0 && rs2 >= 0 && rd >= 0);
+            default:
+                overflow = 0;
+        endcase
+    end
+
+endmodule
--- HW3/reg_file.v
+++ HW3/reg_file.v
@@ -1,1 +1,35 @@
-../HW2/reg_file.v
+module reg_file #(parameter DWIDTH = 32)
+(
+    input                 clk,      // system clock
+    input                 rst,      // system reset
+
+    input  [4 : 0]        rs1_id,   // register ID of data #1
+    input  [4 : 0]        rs2_id,   // register ID of data #2 (if any)
+
+    input                 we,       // if (we) R[rdst_id] <= rdst
+    input  [4 : 0]        rdst_id,  // destination register ID
+    input  [DWIDTH-1 : 0] rdst,     // input to destination register
+
+    output [DWIDTH-1 : 0] rs1,      // register operand #1
+    output [DWIDTH-1 : 0] rs2       // register operand #2 (if any)
+);
+
+    reg [DWIDTH-1:0] R[0:31];
+
+    assign rs1 = R [ rs1_id ];
+    assign rs2 = R [ rs2_id ];
+
+
+    integer i;
+    always @(posedge clk) begin
+        if (rst)
+            for (i = 0; i < 32; i++)
+                R[i] <= 0;
+        else if (we) begin
+            if (rdst_id != 0) begin
+                R[ rdst_id ] <= rdst;
+            end
+        end
+    end
+
+endmodule
